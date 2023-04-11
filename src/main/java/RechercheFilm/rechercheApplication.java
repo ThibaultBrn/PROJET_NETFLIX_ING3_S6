10aac3e4ece828ff99928d04551cca76ec580058
Thibault <thib.brun@gmail.com>
2023-04-11 09:35:21 +0200

    Arborescence fichier fxml et fichier pom mis à jour mais non fonctionnels.

--- src/main/java/RechercheFilm/rechercheApplication.java
+++ src/main/java/RechercheFilm/rechercheApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class rechercheApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(rechercheApplication.class.getResource("rechercheFilm.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(rechercheApplication.class.getResource("resources/org/example/rechercheFilm.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
